eabb2c51a4c420a135e0cff17ba7f1b03e52770b
Daniloms <Danilooo.msilva@gmail.com>
2025-07-22 22:13:42 +0000

    Criei printf e scanf DESAFIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,44 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    
+    //Entrada de Dados
+    
+    char codigo[10];
+    char nome[50];
+    int populacao;
+    float area;
+    float PIB;
+    int PontosTuristicos;
+    
+
     // Cadastro das Cartas:
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%s", &codigo);
+    
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Qual a Area da cidade (em km)");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &PIB);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &PontosTuristicos);
+
+    printf()
+    
+    
+    
+    
+    
+    
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
