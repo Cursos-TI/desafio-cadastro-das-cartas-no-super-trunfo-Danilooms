9689ca6913d85b1f19c4483f7688b294827c5fba
Daniloms <Danilooo.msilva@gmail.com>
2025-07-26 21:28:24 +0000

    DesafioAventureiro

---
+++ FACULDADE/DesafioAventureiro.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char estado1 = 'B';
+         char codigo1[4] = "B01";
+            char nome1[10] = "Fortaleza";
+                int populacao1 = 2686612;
+            float area1 = 312.35;
+        float pib1 = 67.03;
+    int pontos1 = 30;
+       
+
+
+    char estado2 = 'A';
+        char codigo2[4] = "A01";
+            char nome2[10] = "São Paulo";
+                int populacao2 = 12325000;
+            float area2 = 1521.11;
+        float pib2 = 699.28;
+    int pontos2 = 50;
+
+    printf("Carta 1:\n");
+       printf("Estado: %c\n", estado1);
+          printf("Código: %s\n", codigo1);
+            printf("Nome da Cidade: %s\n", nome1);
+               printf("População: %d\n", populacao1);
+           printf("Área: %.2f km²\n", area1);
+          printf("PIB: %.2f bilhões de reais\n", pib1);
+      printf("Pontos Turísticos: %d\n", pontos1);
+printf("A Densidade é: %d\n", Densidade);  
+     
+      int double Densidade = (populacao1 + area1) / 2;
+
+      
+
+
+
+    printf("Carta 2:\n");
+         printf("Estado: %c\n", estado2);
+           printf("Código: %s\n", codigo2);
+              printf("Nome da Cidade: %s\n", nome2);
+               printf("População: %d\n", populacao2);
+             printf("Área: %.2f km²\n", area2);
+         printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turísticos: %d\n", pontos2);
+
+
+    return 0;
+}
